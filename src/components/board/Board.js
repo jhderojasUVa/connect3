19c99ed8a9abb9433955911ed52d8c1562b8b0ee
Jesus Angel Hernandez de Rojas <jesusangel.hernandez@uva.es>
2020-12-29 12:30:18 +0100

    fix(board.js): chips index

--- src/components/board/Board.js
+++ src/components/board/Board.js
@@ -36,9 +36,11 @@ export class Board extends Lightning.Component {
 
 
   _active() {
+    this._chipsChildren = this.tag('Chips').children
     this._generateLines(643, SpaceBetween, 'x', this.tag('HorizontalLines'))
     this._generateLines(965, SpaceBetween, 'y', this.tag('VerticalLines'))
     this._generateChips()
+    //this.checkChips()
   }
 
   _generateLines(size, separation, axis, object) {
@@ -74,7 +76,7 @@ export class Board extends Lightning.Component {
 
   _generateChips() {
     let chips = []
-    for (let i = 0; i < 104; i++) {
+    for (let i = 0; i < 96; i++) {
       chips.push(i)
     }
 
@@ -85,25 +87,25 @@ export class Board extends Lightning.Component {
 
     this.tag('Chips').children = chips.map((item, index) => {
       // random chip color!
-      const chipColor = Colors.Index[Math.floor(Math.random() * Colors.Index.length)]
+      const chipColor = Colors[Colors.Index[Math.floor(Math.random() * Colors.Index.length)]]
 
-      if (index > 8 && (index % 8) == 0) {
+      if (index > 0 && (index % 8) == 0) {
         // new line!
         start.y++
       }
       return {
-        index: index,
         w: ChipSize.w,
         h: ChipSize.h,
         x: index % 8 * SpaceBetween,
         y: start.y * SpaceBetween,
         data: {
+          index,
           x: index % 8,
           y: start.y,
           color: chipColor
         },
         rect: true,
-        texture: Lightning.Tools.getRoundRect(ChipSize.w, ChipSize.h, 40, 0, 0xffff00ff, true, Colors[chipColor]),
+        texture: Lightning.Tools.getRoundRect(ChipSize.w, ChipSize.h, 40, 0, 0xffff00ff, true, chipColor),
         type: Chip
       }
     })
@@ -143,5 +145,57 @@ export class Board extends Lightning.Component {
       x: (index % 8) * SpaceBetween,
       y: (index < 8) ? 0 : Math.floor(index / 8) * SpaceBetween
     })
+    // check chips
+    this.checkChips()
+  }
+
+  checkChips() {
+    console.log(this.checkNextChip(0, 1, 'x'))
+    console.log(this.tag('Chips').children[1])
+    /*this.tag('Chips').children.forEach((element, index) => {
+      //console.log(element)
+      const currentChip = element.data.index
+      // right
+      let right = this.checkNextChip(currentChip, 1, 'x')
+      // left
+      let left = this.checkNextChip(currentChip, -1, 'x')
+      // top
+      //let top = this.checkNextChip(currentChip, -1, 'y')
+      // bottom
+      //let bottom = this.checkNextChip(currentChip, 1, 'y')
+      //console.log(right)
+      console.log(left)
+    });*/
+  }
+
+  checkNextChip(index, direction, axis) {
+    // direction the number you will sum
+    let nextChip
+    switch (axis) {
+      case 'x':
+        if (direction == 1 && index % 8 < 8) {
+          nextChip = this.tag('Chips').children[index + 1] ? this.tag('Chips').children[index + 1].data : undefined
+          console.log(nextChip)
+        } else if (direction == -1 && index % 8 > 0) {
+          nextChip = this.tag('Chips').children[index - 1] ? this.tag('Chips').children[index - 1].data : undefined
+        } else {
+          nextChip = undefined
+        }
+        break
+      case 'y':
+        if (direction == 1) {
+          nextChip = this.tag('Chips').children[index + 8] ? this.tag('Chips').children[index + 8].data : undefined
+        } else if (direction == -1) {
+          nextChip = this.tag('Chips').children[index - 8] ? this.tag('Chips').children[index - 8].data : undefined
+        } else {
+          nextChip = undefined
+        }
+        break
+    }
+    return nextChip ? nextChip.color : undefined
+  }
+
+  clearChips(index, axis) {
+
   }
 }
